b66604d7c1c83d9ead36b83a072e996e37bf6ee1
costfn@gmail.com <costfn@gmail.com>
2014-12-17 08:57:09 -0700

    Cleaned up the source code. May be ready, going to test some more.

---
+++ app/src/main/java/com/dizzylipton/top_4/DBTools.java
@@ -0,0 +1,126 @@
+package com.dizzylipton.top_4;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Patrick on 12/15/2014.
+ */
+public class DBTools extends SQLiteOpenHelper {
+
+    public DBTools(Context appContext) {
+        super(appContext, "top4_book.db", null, 1);
+    }
+
+    public void onCreate(SQLiteDatabase database) {
+
+        String query = "CREATE TABLE top4 (btnId TEXT, contactName TEXT, contactNumber TEXT)";
+        database.execSQL(query);
+
+        for (int i = 0; i < 4; i++) {
+            HashMap<String, String> initMap = new HashMap<>();
+            initMap.put("btnId", Integer.toString(i));
+            initMap.put("contactName", "Not Yet Assigned");
+            initMap.put("contactNumber", "0");
+            insertInitTop4(initMap);
+        }
+
+    }
+
+    public void onUpgrade(SQLiteDatabase database, int version_old, int version_new) {
+        String query = "DROP TABLE IF EXISTS top4";
+        database.execSQL(query);
+        onCreate(database);
+    }
+
+    public ArrayList<HashMap<String, String>> getAllContacts() {
+
+
+        SQLiteDatabase database = this.getWritableDatabase();
+        //String delQuery = "DROP TABLE IF EXISTS top4";
+        //database.execSQL(delQuery);
+        //onCreate(database);
+        //database = this.getWritableDatabase();
+
+        ArrayList<HashMap<String, String>> top4ArrayList;
+        top4ArrayList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM top4";
+
+
+        Cursor cursor = database.rawQuery(selectQuery, null);
+        if(cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> top4Map = new HashMap<>();
+                top4Map.put("btnId", cursor.getString(0));
+                top4Map.put("contactName", cursor.getString(1));
+                top4Map.put("contactNumber", cursor.getString(2));
+
+                top4ArrayList.add(top4Map);
+            } while(cursor.moveToNext());
+        }
+        return top4ArrayList;
+    }
+    public void insertInitTop4(HashMap<String, String> queryValues) {
+
+        // This method should only be called once to initialize the database
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("btnId", queryValues.get("btnId"));
+        values.put("contactName", queryValues.get("contactName"));
+        values.put("contactNumber", queryValues.get("contactNumber"));
+
+        database.insert("top4", null, values);
+        database.close();
+    }
+
+    public void dropDatabase(SQLiteDatabase database) {
+        String query = "DROP TABLE IF EXISTS top4";
+        database.execSQL(query);
+        onCreate(database);
+    }
+
+    public int updateTop4(HashMap<String, String> queryValues) {
+
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("contactName", queryValues.get("contactName"));
+        values.put("contactNumber", queryValues.get("contactNumber"));
+
+        return database.update("top4", values, "btnId" + " = ?", new String[]{ queryValues.get("btnId")});
+    }
+
+    public void deleteTop4(String id) {
+        // This will never be used, places in here for reference
+        SQLiteDatabase database = this.getWritableDatabase();
+        String deleteQuery = "DELETE FROM top4 WHERE btnId ='" + id + "'";
+        database.execSQL(deleteQuery);
+    }
+
+    public String getContactNumber(String id) {
+
+        String contactNumber = "";
+        SQLiteDatabase database = this.getReadableDatabase();
+        String selectQuery = "SELECT contactNumber from top4 where btnId='" + id + "'";
+        Cursor cursor = database.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                contactNumber = cursor.getString(0);
+            } while (cursor.moveToNext());
+        }
+
+        return contactNumber;
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/dizzylipton/top_4/MainActivity.java
@@ -0,0 +1,156 @@
+package com.dizzylipton.top_4;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    private final static int PICK_CONTACT = 10;
+    private final static String DEBUG_TAG = "PAT";
+
+    Button[] callButton = new Button[4];
+    int callButtonId;
+
+    final int[] btnR = new int[]{
+            R.id.callButton1,
+            R.id.callButton2,
+            R.id.callButton3,
+            R.id.callButton4,
+    };
+
+    DBTools dbTools = new DBTools(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<HashMap<String, String>> top4List = dbTools.getAllContacts();
+
+        for (int i = 0; i < top4List.size(); i++) {
+
+            callButton[i] = (Button) findViewById(btnR[i]);
+            callButton[i].setText(top4List.get(i).get("contactName"));
+
+            callButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String number = "";
+                    for (int i = 0; i < 4; i++) {
+                        if (v.getId() == btnR[i]) {
+                            number = dbTools.getContactNumber(Integer.toString(i));
+                            /**
+                             * Here is were we make the phone call.
+                             * Commented out so I don't butt (test) dial anyone
+                             */
+                            //Intent callIntent = new Intent(Intent.ACTION_CALL);
+                            //callIntent.setData(Uri.parse("tel:" + number));
+                            //startActivity(callIntent);
+                            break;
+
+                        }
+                    }
+
+                    Log.e(DEBUG_TAG, number);
+                }
+            });
+
+            callButton[i].setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Log.d(DEBUG_TAG, "Long CLick");
+                    doLaunchContactPicker(v);
+                    return false;
+                }
+            });
+        }
+    }
+
+    public void doLaunchContactPicker(View sender) {
+        Log.d(DEBUG_TAG, "Launch Contact");
+
+        for (int i = 0; i < 4; i++) {
+            if (btnR[i] == sender.getId()) {
+                callButtonId = i;
+                break;
+            }
+        }
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, PICK_CONTACT);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String number = "";
+        String name = "";
+
+
+        if (resultCode == RESULT_OK && requestCode == PICK_CONTACT) {
+
+            Cursor cursor = null;
+
+            try {
+                Uri result = data.getData();
+                String id = result.getLastPathSegment();
+
+                String[] projections = {Phone._ID,
+                        Phone.NUMBER,
+                        ContactsContract.Contacts.DISPLAY_NAME,
+                };
+
+                cursor = getContentResolver().query(ContactsContract.Data.CONTENT_URI,
+                        projections,
+                        Phone._ID + "=?" + " AND " + ContactsContract.Contacts.Data.MIMETYPE + "='" + Phone.CONTENT_ITEM_TYPE + "'",
+                        new String[]{String.valueOf(id)},
+                        null);
+
+                int numberIdx = cursor.getColumnIndex(Phone.DATA);
+
+
+                if (cursor.moveToFirst()) {
+                    number = cursor.getString(numberIdx);
+                    name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+
+        try {
+            Button callButton = (Button) findViewById(btnR[callButtonId]);
+            callButton.setText(name);
+            HashMap<String, String> updateMap = new HashMap<>();
+            updateMap.put("btnId", Integer.toString(callButtonId));
+            updateMap.put("contactNumber", number);
+            updateMap.put("contactName", name);
+
+            dbTools.updateTop4(updateMap);
+            //updateButtons();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
